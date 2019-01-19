38f5d8af7f5bd627a3fb633675e03c84ddf5d89f
juan-carlos-correa <juan.correa.herrera@gmail.com>
2019-01-19 15:41:23 -0600

    develop | add redux-logger

--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk),
+  applyMiddleware(
+    logger,
+    thunk,
+  ),
 );
 
 console.log('initial state', store.getState());
