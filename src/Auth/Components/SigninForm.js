5034b019167dbc5545a8b5c4eaebb2dfdf3a7fc1
juan-carlos-correa <juan.correa.herrera@gmail.com>
2019-02-04 15:10:17 -0600

    develop | update SigninForm input values reference

--- src/Auth/Components/SigninForm.js
+++ src/Auth/Components/SigninForm.js
@@ -11,61 +11,71 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const SigninForm = ({
-  email,
-  password,
+  values,
   errors,
   isSigninLoading,
   handleChange,
   handleBlur,
   handleFocus,
   handleSubmit,
-}) => (
-  <Form onSubmit={handleSubmit}>
-    <FormGroup>
-      <Label for="email">Email</Label>
-      <Input
-        id="email"
-        name="email"
-        type="email"
-        placeholder="john.doe@mail.com"
-        value={email}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        onFocus={handleFocus}
-        invalid={!!errors.email}
-      />
-      <FormFeedback>{errors.email}</FormFeedback>
-    </FormGroup>
+}) => {
+  const { email, password } = values;
 
-    <FormGroup>
-      <Label for="password">Contraseña</Label>
-      <Input
-        id="password"
-        name="password"
-        type="password"
-        placeholder="******"
-        value={password}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        onFocus={handleFocus}
-        invalid={!!errors.password}
-      />
-      <FormFeedback>{errors.password}</FormFeedback>
-    </FormGroup>
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="email">Email</Label>
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="john.doe@mail.com"
+          value={email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+          invalid={!!errors.email}
+        />
+        <FormFeedback>{errors.email}</FormFeedback>
+      </FormGroup>
 
-    <FormGroup className="text-center">
-      <Button disabled={isSigninLoading} color="primary">ENVIAR</Button>
-    </FormGroup>
+      <FormGroup>
+        <Label for="password">Contraseña</Label>
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="******"
+          value={password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+          invalid={!!errors.password}
+        />
+        <FormFeedback>{errors.password}</FormFeedback>
+      </FormGroup>
 
-    <FormGroup className="text-center">
-      <Link to='/signup'>Crear cuenta</Link>
-    </FormGroup>
-  </Form>
-);
+      <FormGroup className="text-center">
+        <Button disabled={isSigninLoading} color="primary">ENVIAR</Button>
+      </FormGroup>
+
+      <FormGroup className="text-center">
+        <Link to='/signup'>Crear cuenta</Link>
+      </FormGroup>
+    </Form>
+  );
+}
 
 SigninForm.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
   isSigninLoading: PropTypes.bool.isRequired,
+  values: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleBlur: PropTypes.func.isRequired,
+  handleFocus: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
 }
 
 export default SigninForm;
