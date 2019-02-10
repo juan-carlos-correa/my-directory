dbe512aa26604c9d52de624ec68f387300a35dc6
juan-carlos-correa <juan.correa.herrera@gmail.com>
2019-02-10 12:19:08 -0600

    develop | add firebase function for create user

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.createUser = functions.firestore
+  .document('users/{userId}')
+  .onCreate((snap, context) => {
+    const newValue = snap.data();
+
+    // access a particular field as you would any JS property
+    const name = newValue.name;
+    const password = newValue.defaultPassword;
+    const isAdmin = newValue.isAdmin;
+
+    if (isAdmin) {
+      console.log('User created is admin');
+      return false;
+    }
+
+    return admin.auth().createUser({ name, password })
+      .then(() => console.log('user created'))
+      .catch((err) => console.log(err));
+  });
